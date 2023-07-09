a50e62d8f31300a804996ef49fa420cf25d916f7
eldorPulatov <55455535+eldorPulatov@users.noreply.github.com>
2023-07-09 19:04:58 +0300

    Add files via upload

---
+++ С#/Практическая 2/WordCounterApp/WordCounterApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using WordCounterLibrary;
+
+namespace WordCounterApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string inputFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+            string outputFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "output.txt");
+            string timeFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "time.txt");
+
+            try
+            {
+                string content = File.ReadAllText(inputFile);
+
+                Assembly assembly = Assembly.LoadFrom("WordCounterLibrary.dll");
+                Type wordCounterType = assembly.GetType("WordCounterLibrary.WordCounter");
+                object wordCounterInstance = Activator.CreateInstance(wordCounterType);
+                MethodInfo countWordsMethodPrivate = wordCounterType.GetMethod("CountWords", BindingFlags.Instance | BindingFlags.NonPublic);
+
+                // Измерение времени приватного метода
+                Stopwatch privateMethodStopwatch = Stopwatch.StartNew();
+                Dictionary<string, int> privateMethodResult = (Dictionary<string, int>)countWordsMethodPrivate.Invoke(wordCounterInstance, new object[] { content });
+                privateMethodStopwatch.Stop();
+                TimeSpan privateMethodTime = privateMethodStopwatch.Elapsed;
+
+                WordCounter wordCounter = new WordCounter();
+
+                // Измерение времени публичного метода
+                Stopwatch publicMethodStopwatch = Stopwatch.StartNew();
+                Dictionary<string, int> publicMethodResult = wordCounter.CountWordsMultiThreaded(content);
+                publicMethodStopwatch.Stop();
+                TimeSpan publicMethodTime = publicMethodStopwatch.Elapsed;
+
+
+                // Запись результатов в файл output.txt (отсортировано по убыванию)
+                using (StreamWriter writer = new StreamWriter(outputFile))
+                {
+                    writer.WriteLine("Результаты подсчета слов:");
+                    WriteWordCountDescending(writer, publicMethodResult);
+                }
+
+                // Запись результатов времени выполнения в файл time.txt
+                using (StreamWriter writer = new StreamWriter(timeFile))
+                {
+                    writer.WriteLine("Время выполнения приватного метода: " + privateMethodTime);
+                    writer.WriteLine("Время выполнения публичного метода: " + publicMethodTime);
+                }
+
+                Console.WriteLine("Результаты записаны в файлы.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        static void WriteWordCountDescending(StreamWriter writer, Dictionary<string, int> wordCount)
+        {
+            foreach (var pair in wordCount.OrderByDescending(w => w.Value))
+            {
+                writer.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+        }
+    }
+}
---
+++ С#/Практическая 2/WordCounterLibrary/WordCounterLibrary/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Concurrent;
+
+namespace WordCounterLibrary
+{
+    public class WordCounter
+    {
+        private Dictionary<string, int> CountWords(string text)
+        {
+            char[] separators = new char[] { ' ', ',', '.', '!', '?', ';', ':', '-', '\n', '\r', '\t' };
+            string[] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                if (wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+                else
+                {
+                    wordCount[word] = 1;
+                }
+            }
+
+            return wordCount;
+        }
+
+        public Dictionary<string, int> CountWordsMultiThreaded(string text)
+        {
+            text = text.Replace("\"", "");
+            // Создаем параллельный словарь для безопасной работы с счетчиком слов из нескольких потоков
+            ConcurrentDictionary<string, int> wordCount = new ConcurrentDictionary<string, int>();
+ 
+            string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Parallel.ForEach(words, word =>
+            {
+                wordCount.AddOrUpdate(word, 1, (key, count) => count + 1);
+            });
+
+            // Преобразуем параллельный словарь в наш обычный словарь
+            Dictionary<string, int> result = new Dictionary<string, int>(wordCount);
+
+            return result;
+        }
+    }
+}
