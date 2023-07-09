b561e59f0ed3dd37485159bdaa4961c0a8b92f4c
eldorPulatov <55455535+eldorPulatov@users.noreply.github.com>
2023-07-09 19:06:11 +0300

    Add files via upload

---
+++ С#/Практическая 3/ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string url = "https://localhost:7190/api/Text";
+
+        string inputFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+        string text = File.ReadAllText(inputFile);
+
+        Console.WriteLine(text);
+
+        //    ServicePointManager.ServerCertificateValidationCallback += ValidateCertificate;
+
+        //    using (HttpClient client = new HttpClient())
+        //    {
+        //        var response = client.PostAsJsonAsync(url, text).Result;
+
+        //        if (response.IsSuccessStatusCode)
+        //        {
+        //            var result = response.Content.ReadAsStringAsync().Result;
+        //            Console.WriteLine(result);
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("Error: " + response.StatusCode);
+        //        }
+        //    }
+        //}
+
+        //private static bool ValidateCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        //{
+        //    // верификации, которая всегда принимает сертификаты
+        //    return true;
+        //}
+    }
+}
---
+++ С#/Практическая 3/WebApi/WebApplication2/Controllers/TextController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WordCounterLibrary;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TextController : Controller
+    {
+        [HttpGet]
+        public string Get() 
+        {
+            var text = "Hi there!";
+            return text;
+        }
+
+        [HttpPost]
+        public ActionResult<Dictionary<string, int>> Post([FromBody] string text) 
+        {
+            WordCounter wordCounter = new WordCounter();
+            Dictionary<string, int> result = wordCounter.CountWordsMultiThreaded(text);
+            return Ok(result);
+        }
+    }
+}
