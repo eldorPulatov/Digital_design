2a6e5116d2691eff3b721116673bd38f7fdfe4ba
eldorPulatov <55455535+eldorPulatov@users.noreply.github.com>
2023-07-09 19:03:46 +0300

    Add files via upload

---
+++ С#/Практическая 1/WordCounterApp/WordCounterApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+
+namespace WordCounterApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            ReflectionExample();
+
+        }
+
+        private static void ReflectionExample()
+        {
+            string inputFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+            string outputFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "output.txt");
+
+            try
+            {
+                string content = File.ReadAllText(inputFile);
+
+                Assembly assembly = Assembly.LoadFrom("WordCounterLibrary.dll");
+                Type wordCounterType = assembly.GetType("WordCounterLibrary.WordCounter");
+                object wordCounterInstance = Activator.CreateInstance(wordCounterType);
+
+                MethodInfo countWordsMethod = wordCounterType.GetMethod("CountWords", BindingFlags.Instance | BindingFlags.NonPublic);
+                Dictionary<string, int> wordCount = (Dictionary<string, int>)countWordsMethod.Invoke(wordCounterInstance, new object[] { content });
+
+                var sortedWords = wordCount.OrderByDescending(w => w.Value);
+
+                using (StreamWriter writer = new StreamWriter(outputFile))
+                {
+                    foreach (var word in sortedWords)
+                    {
+                        writer.WriteLine($"{word.Key}\t{word.Value}");
+                    }
+                }
+
+                Console.WriteLine("Результат записан в файл.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ С#/Практическая 1/WordCounterLibrary/WordCounterLibrary/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace WordCounterLibrary
+{
+    public class WordCounter
+    {
+        private Dictionary<string, int> CountWords(string text)
+        {
+            char[] separators = new char[] { ' ', ',', '.', '!', '?', ';', ':', '-', '\n', '\r', '\t' };
+            string[] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                if (wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+                else
+                {
+                    wordCount[word] = 1;
+                }
+            }
+
+            return wordCount;
+        }
+    }
+}
